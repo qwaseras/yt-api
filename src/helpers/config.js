c75d61b36ca8c54f4dd2aef2ffe8e9ac5f4ab9e7
qwaseras <qwqwqaqs@gmail.com>
2020-09-22 16:26:50 +0300

    config/logic/fix: hide api key

--- src/helpers/config.js
+++ src/helpers/config.js
@@ -1,4 +1,4 @@
-export const GOOGLE_API_KEY = 'AIzaSyCP-aPry4H5KiWkGIK98P-fWIkrY0K1PIQ';
+export const GOOGLE_API_KEY = process.env.GOOGLE_API_KEY;
 export const YT_SEARCH_API_URL = 'https://www.googleapis.com/youtube/v3/search';
 export const YT_PARSER_REGEX = /youtu(?:.*\/v\/|.*v=|\.be\/)([A-Za-z0-9_-]{11})/
 export const YT_EMBED_VIDEO_URL = 'https://www.youtube.com/embed/'
