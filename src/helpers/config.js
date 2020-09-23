ea0da0559e6a917700bc2bbf8dd5ea0eb553857b
qwaseras <qwqwqaqs@gmail.com>
2020-09-23 12:35:04 +0300

    log api key

--- src/helpers/config.js
+++ src/helpers/config.js
@@ -1,4 +1,5 @@
 export const GOOGLE_API_KEY = process.env.GOOGLE_API_KEY;
+console.log(`using API key: ${GOOGLE_API_KEY}`)
 export const YT_SEARCH_API_URL = 'https://www.googleapis.com/youtube/v3/search';
 export const YT_PARSER_REGEX = /youtu(?:.*\/v\/|.*v=|\.be\/)([A-Za-z0-9_-]{11})/
 export const YT_EMBED_VIDEO_URL = 'https://www.youtube.com/embed/'
